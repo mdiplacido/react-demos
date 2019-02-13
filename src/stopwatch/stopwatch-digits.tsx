4091b7ff04e3846e0fabb39b51ac950f62fdc343
Marco DiPlacido <marcodiplacido@Marcos-MacBook-Pro.local>
2019-02-12 22:20:10 -0500

    working demo

--- src/stopwatch/stopwatch-digits.tsx
+++ src/stopwatch/stopwatch-digits.tsx
@@ -4,12 +4,13 @@ export interface StopwatchDigitsProps {
     hours: number;
     minutes: number;
     seconds: number;
+    milliseconds: number;
 }
 
 const StopwatchDigits = (props: StopwatchDigitsProps) => {
     return (
         <div>
-            {("0" + props.hours).slice(-2)} : {("0" + props.minutes).slice(-2)} : {("0" + props.seconds).slice(-2)}
+            {props.hours < 10 ? "0" + props.hours : props.hours} : {("0" + props.minutes).slice(-2)} : {("0" + props.seconds).slice(-2)}.{("00" + props.milliseconds).slice(-3)}
         </div>
     )
 }
--- src/stopwatch/stopwatch.container.tsx
+++ src/stopwatch/stopwatch.container.tsx
@@ -1,14 +1,113 @@
-import React, { Component } from 'react'
-import StopwatchDigits from './stopwatch-digits';
+import React, { Component } from "react";
 
-export default class StopwatchContainer extends Component {
+import StopwatchDigits from "./stopwatch-digits";
+
+interface StopWatchState {
+  lastMark?: Date;
+  isRunning: boolean;
+  hours: number;
+  minutes: number;
+  seconds: number;
+  milliseconds: number;
+  totalElapsedMilliseconds: number;
+  currentElapsedMilliseconds: number;
+}
+
+const FramesPerSecond = 24;
+const MillisecondsInHour = 60 * 60 * 1000;
+const MillisecondsInMinute = 60 * 1000;
+const MillisecondsInSecond = 1000;
+
+export default class StopwatchContainer extends Component<{}, StopWatchState> {
+  private timerHandle?: number;
+
+  public state: Readonly<StopWatchState> = {
+    isRunning: false,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    milliseconds: 0,
+    totalElapsedMilliseconds: 0,
+    currentElapsedMilliseconds: 0
+  };
+
+  componentWillUnmount(): void {
+    // show with and without cleanup
+    clearTimeout(this.timerHandle);
+  }
+
   render() {
     return (
       <div>
         <h1>Hewwow, stopwatch!</h1>
 
-        <StopwatchDigits hours={1} minutes={34} seconds={59} />
+        <StopwatchDigits 
+          hours={this.state.hours} 
+          minutes={this.state.minutes} 
+          seconds={this.state.seconds}
+          milliseconds={this.state.milliseconds} />
+
+        <button onClick={this.toggle}>{this.state.isRunning ? "Stop" : "Start"}</button>
       </div>
     )
+  }
+
+  private toggle = () => {
+    // start or stop the timer
+    this.setState(prev => ({
+      isRunning: !prev.isRunning,
+      lastMark: new Date()
+    }), () => {
+      if (!this.state.isRunning) {
+        // we rollup the current into total 
+        this.setState(prev => ({
+          totalElapsedMilliseconds: prev.currentElapsedMilliseconds + prev.totalElapsedMilliseconds,
+          currentElapsedMilliseconds: 0
+        }));
+      } else {
+        this.onTick();
+      }
+    });
+  }
+
+  private onTick(lastMark?: Date): void {
+    if (!this.state.isRunning) {
+      // if we are not running then the timer fired just before the user canceled 
+      return;
+    }
+
+    const mark = lastMark || this.state.lastMark;
+    this.updateElapsedTime(mark);
+
+    this.timerHandle = window.setTimeout(() => {
+      this.onTick(mark);
+    }, 1000 / FramesPerSecond);
+  }
+
+  private updateElapsedTime(mark?: Date): void {
+    if (!mark) {
+      throw new Error("invalid state");
+    }
+
+    const now = new Date();
+    const currentElapsed = now.getTime() - mark.getTime();
+    let elapsed = currentElapsed + this.state.totalElapsedMilliseconds;
+
+    const hours = Math.floor(elapsed / MillisecondsInHour);
+    elapsed -= hours * MillisecondsInHour;
+
+    const minutes = Math.floor(elapsed / MillisecondsInMinute);
+    elapsed -= minutes * MillisecondsInMinute;
+
+    const seconds = Math.floor(elapsed / MillisecondsInSecond);
+    elapsed -= seconds * MillisecondsInSecond;
+
+    this.setState({
+      hours,
+      minutes,
+      seconds,
+      milliseconds: elapsed,
+      currentElapsedMilliseconds: currentElapsed
+    });
   }
 }
